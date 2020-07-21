43f58bd8bdc765082ea11c4259e2314002473048
Bondarenko Vladislav <bond.zonda777@gmail.com>
2020-07-21 21:19:39 +0300

    first_commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://lh3.googleusercontent.com/proxy/JNJ0WZb_IreinqJp8s2gKmAIqNqRtpQOFVHVGSjpenjvMN_MKetEdZt0z9Zs7WmSxm1HCX8lkNLtB5s28bmPVDeYKQuWujv0oMPl5I2ZA_arqlDh" />
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg"></img>
+        </div>
+        <div>
+          avatar+description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+    
+    )
+}
+
+export default Profile
